dccb8fc07b771b7eae124f4b012ab35269edf815
vitorCamarotto <vitorranacamarotto@gmail.com>
2024-03-18 10:50:22 -0300

    Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-app>
+      <v-app-bar
+        app
+        elevation="5"
+        class="navbar text-white"
+        data-test="navbar"
+      >
+        <v-spacer />
+
+      </v-app-bar>
+
+      <v-main>
+        <slot />
+      </v-main>
+    </v-app>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,5 @@
 <template>
-  <div>
-    <v-app>
-      <v-app-bar
-        app
-        elevation="5"
-        class="navbar text-white"
-        data-test="navbar"
-      >
-        <v-spacer />
-
-      </v-app-bar>
-
-      <v-main>
-        <slot />
-      </v-main>
-    </v-app>
-  </div>
+  <h1>
+    hey
+  </h1>
 </template>
