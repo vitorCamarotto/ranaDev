dcf5c32260ee443c504ad5368a768a058d34e9d3
vitorCamarotto <vitorranacamarotto@gmail.com>
2024-04-11 07:19:32 -0300

    Add hover effects to connect page

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -5,24 +5,30 @@
         {{ $t('contact.title') }}
       </h1>
 
-      <div class="mt-4 flex flex-col items-center">
+      <div
+        class="mt-4 flex flex-col items-center px-6 border-x-2 border-white
+          hover:border-red-500 hover:rounded-2xl transition-all duration-500
+        "
+      >
         <Icon
           name="material-symbols-light:mail-outline-rounded"
           class=""
-          size="50px"
+          size="40px"
         />
         <p>
           vitorranacamarotto@gmail.com
         </p>
       </div>
 
-      <div class="flex justify-center gap-6">
+      <div class="flex justify-center gap-8">
         <div class="flex flex-col mt-10">
           <div class="self-center justify-self-center">
               <a href="https://github.com/vitorCamarotto" target="_blank">
               <Icon
                 name="akar-icons:github-outline-fill"
-                class="icon-hover-effect cursor-pointer hover:text-red-500"
+                class="cursor-pointer w-full px-2 border-x-2 border-white hover:border-red-500
+                  hover:rounded-2xl hover:text-red-500 transition-all duration-500
+                "
                 size="50px"
               />
             </a>
@@ -34,7 +40,9 @@
               <a href="https://www.linkedin.com/in/vitor-rana-camarotto/" target="_blank">
               <Icon
                 name="uit:linkedin-alt"
-                class="self-center cursor-pointer border-2 border-white rounded hover:text-red-500 hover:border-red-500"
+                class="cursor-pointer w-full px-2 border-x-2 hover:border-red-500 hover:rounded-2xl
+                  transition-all duration-500 hover:text-red-500
+                "
                 size="50px"
               />
             </a>
