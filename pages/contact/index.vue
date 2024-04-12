1053c1839fd93c9e8f4826b875dafd6c60e4c31f
vitorCamarotto <vitorranacamarotto@gmail.com>
2024-04-12 17:57:11 -0300

    Improve connect page

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,50 +1,74 @@
 <template>
   <div class="p-2 flex flex-col">
-    <div class="self-center justify-self-center">
+    <div class="self-center justify-self-center flex items-center flex-col">
       <h1 class="text-center">
         {{ $t('contact.title') }}
       </h1>
 
       <div
-        class="mt-4 flex flex-col items-center px-6 border-x-2 border-transparent transition-all duration-500"
-        :class="emailHoverClass"
+        class="
+          flex justify-center items-center mt-10 border-t-2
+          rounded-full h-20 w-20 rotate-180 transition-all duration-[1000ms]
+        "
+        :class="hoverClass"
+        @click="copyEmailToClipboard"
+        @mouseenter="activateSun"
+        @mouseleave="deActivateSun"
       >
         <Icon
           name="material-symbols-light:mail-outline-rounded"
-          class=""
+          class="rotate-180"
           size="40px"
         />
-        <p>
-          vitorranacamarotto@gmail.com
-        </p>
       </div>
 
-      <div class="flex justify-center gap-8">
+      <p :class="showCopiedTextClass" class="opacity-0 transition-all duration-1000">
+        Email copied
+      </p>
+
+      <div
+        class="mt-8 h-4 w-4 rounded-full transition-all duration-[2000ms]"
+        :class="centerSunClass"
+      ></div>
+
+      <div class="flex justify-center gap-24">
         <div class="flex flex-col mt-10">
           <div class="self-center justify-self-center">
+            <div
+              class="border-t-2 rounded-full h-20 w-20 rotate-45 p-2 transition-all duration-[1000ms]"
+              :class="hoverClass"
+              @mouseenter="activateSun"
+              @mouseleave="deActivateSun"
+            >
               <a href="https://github.com/vitorCamarotto" target="_blank">
-              <Icon
-                name="akar-icons:github-outline-fill"
-                class="cursor-pointer w-full px-2 border-x-2 border-transparent transition-all duration-500"
-                :class="hoverClass"
-                size="50px"
-              />
-            </a>
-              </div>
-          </div>
-
-          <div class="flex flex-col mt-10">
-            <div class="self-center justify-self-center">
-              <a href="https://www.linkedin.com/in/vitor-rana-camarotto/" target="_blank">
-              <Icon
-                name="uit:linkedin-alt"
-                class="cursor-pointer w-full px-2 border-x-2 border-transparent transition-all duration-500"
-                :class="hoverClass"
-                size="50px"
-              />
-            </a>
+                <Icon
+                  name="ph:github-logo-thin"
+                  class="cursor-pointer w-full -rotate-45"
+                  size="50px"
+                />
+              </a>
             </div>
           </div>
+        </div>
+
+        <div class="flex flex-col mt-10">
+          <div class="self-center justify-self-center">
+            <div
+              class="border-t-2 rounded-full h-20 w-20 -rotate-45 p-2 transition-all duration-[1000ms]"
+              :class="hoverClass"
+              @mouseenter="activateSun"
+              @mouseleave="deActivateSun"
+            >
+              <a href="https://www.linkedin.com/in/vitor-rana-camarotto/" target="_blank">
+                <Icon
+                  name="uit:linkedin-alt"
+                  class="cursor-pointer w-full rotate-45"
+                  size="50px"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -52,26 +76,42 @@
 
 <script setup>
 const colorMode = useColorMode()
+const showCopiedText = ref(false)
+const sunActivated = ref(false)
 
-const fillColor = computed(() => {
-  if (colorMode.value === 'light') {
-    return 'black'
-  } else {
-    return 'white'
-  }
-})
+const darkHoverClass = 'border-white hover:shadow-lg hover:shadow-orange-700 hover:text-orange-600 hover:border-orange-600'
+const lightHoverClass = 'border-black hover:shadow-lg hover:shadow-cyan-400 hover:text-cyan-400 hover:border-cyan-400'
 
 const hoverClass = computed(() => {
-  const darkHoverClass = 'hover:border-red-500 hover:rounded-2xl hover:text-red-500'
-  const lightHoverClass = 'hover:border-cyan-400 hover:rounded-2xl hover:text-cyan-400'
-
   return colorMode.value === 'light' ? lightHoverClass : darkHoverClass
 })
 
-const emailHoverClass = computed(() => {
-  const darkHoverClass = 'hover:border-red-500 hover:rounded-2xl'
-  const lightHoverClass = 'hover:border-cyan-400 hover:rounded-2xl'
+const centerSunClass = computed(() => {
+  if (sunActivated.value) {
+    return colorMode.value === 'light' ? 'shadow-cyan-400 shadow-inner' : 'shadow-orange-700 shadow-inner'
+  }
 
-  return colorMode.value === 'light' ? lightHoverClass : darkHoverClass
+  return colorMode.value === 'light' ? 'bg-black' : 'bg-white'
 })
+
+const activateSun = () => {
+  sunActivated.value = true
+}
+
+const deActivateSun = () => {
+  sunActivated.value = false
+}
+
+const showCopiedTextClass = computed(() => {
+  return showCopiedText.value ? 'opacity-100' : 'opacity-0'
+})
+
+const copyEmailToClipboard = () => {
+  navigator.clipboard.writeText('vitorranacamarotto@gmail.com')
+  showCopiedText.value = true
+
+  setTimeout(() => {
+    showCopiedText.value = false
+  }, 2000)
+}
 </script>
