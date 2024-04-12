1e19f05e9c6357feff667cb91eb512b896d39224
vitorCamarotto <vitorranacamarotto@gmail.com>
2024-04-12 14:37:43 -0300

    Add about new look

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="p-2 flex flex-col">
     <div class="self-center justify-self-center md:max-w-[540px]">
-      <h1>
+      <h1 class="flex flex-col items-center">
         {{ $t('about.title') }}
       </h1>
 
-      <div class="group w-fit mt-8">
+      <div class="group w-auto mt-8 flex flex-col items-center">
         <div
-          class="text-center flex items-center h-20 w-20 border-l-2 rounded-full
+          class="
+            text-center flex items-center h-20 w-20 border-l-2 rounded-full
             group-hover:rotate-[270deg] transition-all duration-[5000ms]
           "
+          :class="handleMoonClass"
         >
           <h2 class="ms-1 group-hover:-rotate-[270deg] transition-all duration-[5000ms] ">
             {{ $t('about.me') }}
           </h2>
         </div>
 
-        <div class="opacity-0 group-hover:opacity-100 transition-all duration-[1000ms]">
+        <div class="opacity-0 group-hover:opacity-100 h-0 group-hover:h-full group-hover:mb-48 transition-all duration-[1000ms]">
           <p class="mt-4">
             {{ $t('about.description1') }}
           </p>
@@ -25,16 +27,17 @@
             {{ $t('about.description3') }}
           </p>
 
-          <p class="mt-2" v-html="$t('about.description4', { link: universityLink })" />
-
+          <p class="mt-2 h-0 group-hover:h-fit" v-html="$t('about.description4', { link: universityLink })" />
         </div>
       </div>
 
-      <div class="group w-fit mt-8">
+      <div class="group w-fit mt-8 flex flex-col items-center">
         <div
-          class="text-center flex items-center h-20 w-20 border-l-2 rounded-full
+          class="
+            text-center flex items-center h-20 w-20 border-l-2 rounded-full
             group-hover:rotate-[270deg] transition-all duration-[5000ms]
           "
+          :class="handleMoonClass"
         >
           <h2 class="ms-1 group-hover:-rotate-[270deg] transition-all duration-[5000ms] ">
             {{ $t('about.professionalExp') }}
@@ -77,4 +80,11 @@ const universityLink = ref(`<a href='https://edurank.org/engineering/la/' target
 const capimLink = ref(`<a href='https://capim.com.br' target='_blank' class='${urlColor}''>Fintech Startup in the Dental Industry</a>`)
 
 import { capimStack } from '../../constants/capim-stack.js'
+
+const moonDarkClass = 'border-white group-hover:shadow-inner group-hover:shadow-orange-600'
+const moonLightClass = 'border-black group-hover:shadow-inner group-hover:shadow-cyan-500'
+
+const handleMoonClass = computed(() => {
+  return colorMode.value === 'light' ? moonLightClass : moonDarkClass
+})
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   darkMode: 'class',
   variants: {
   extend: {
-      display: ["group-hover"],
+    display: ["group-hover"],
   },
 },
 }
